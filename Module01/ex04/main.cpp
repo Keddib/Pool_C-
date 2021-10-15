3c97efe7422aa91e50e17ecacf2cb3966293acf3
Khalid Eddib <keddib@s1r6p3.1337.ma>
2021-10-15 09:31:15 +0100

    ignore

--- Module01/ex04/main.cpp
+++ Module01/ex04/main.cpp
@@ -10,15 +10,12 @@ int ft_error()
 void	replaceLine(std::string &l, const std::string &s1, const std::string &s2)
 {
 	typedef std::string::iterator iter;
-	while (1)
+	size_t found = l.find(s1);
+	while (found != std::string::npos)
 	{
-		size_t found = l.find(s1);
-		if (found == std::string::npos)
-			break ;
-		iter isert = l.erase(l.begin() + found, l.end() + s1.size());
+		iter isert = l.erase(l.begin() + found, l.begin() + found + s1.size());
 		l.insert(isert, s2.begin(), s2.end());
-		std::cout << l + "\n";
-		
+		found = l.find(s1);
 	}
 }
 
@@ -26,21 +23,25 @@ int main(int argc, char **argv)
 {
 	if (argc != 4)
 		return ft_error();
-	std::fstream inFile;
+	std::ifstream inFile;
+	std::ofstream outFile;
 	std::string Line;
 	inFile.open(argv[1]);
-	if (inFile.is_open())
+	outFile.open(std::string(argv[1]) + ".replaced");
+	if (inFile.is_open() && outFile.is_open())
 	{
 		std::string Line;
+		getline(inFile, Line);
 		while (!inFile.eof())
 		{
+			if (!Line.empty())
+				replaceLine(Line, argv[2], argv[3]);
+			outFile << Line + '\n';
 			Line.clear();
 			getline(inFile, Line);
-			replaceLine(Line, argv[2], argv[3]);
-			//put line on file
-			// std::cout << Line + '\n';
 		}
 		inFile.close();
+		outFile.close();
 	}
 	else
 		std::cout << "Could not open file \n";
